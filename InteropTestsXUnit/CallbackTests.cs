e1a57115c010eeea3e3031071159c3511b462540
Elliot Morris <Elliot.Morris@magnopus.com>
2025-11-05 14:37:38 +0000

    Remove hope-based GC tests
    Im a little surprised the native interop is so difficult to make deterministic in failure cases from the C# side, I thought there'd be a special managed exception or something like that. :shrug:
    
    We _can_ make the interfaces resistant to this, but it involves making sure unmanaged calls to collected managed objects can never happen, which involves pinning the managed adapters.
    
    This is a decision to be made, because you can leave this in control of the developers, or in control of the generated interop layer. There's appeals to both.

--- InteropTestsXUnit/CallbackTests.cs
+++ InteropTestsXUnit/CallbackTests.cs
@@ -4,23 +4,24 @@ using Csp;
 using Microsoft.Diagnostics.NETCore.Client;
 using System.Diagnostics;
 using System.IO;
-
+using System.Threading.Tasks;
 
 public class CallbackTests
 {
+
     [Fact]
-    public void Callbacks()
+    public async Task Callbacks()
     {
         /* Test that our callback adaptations function
          * We test 2 here, because we have a static buffer under the hood (swig IL2CPP adaptation), so 
          * it's possible state could be getting mangled. */
-        LogSystem logSystem = new LogSystem();
+        using LogSystem logSystem = new LogSystem();
         Assert.True(logSystem != null);
 
         LogLevel? capturedLevel1 = null;
         string? capturedMessage1 = null;
 
-        ConnectedSpacesPlatformDotNet.LogCallback callback1 = new ConnectedSpacesPlatformDotNet.LogCallback((logLevel, message) =>
+        using ConnectedSpacesPlatformDotNet.LogCallback callback1 = new ConnectedSpacesPlatformDotNet.LogCallback((logLevel, message) =>
         {
             capturedLevel1 = logLevel;
             capturedMessage1 = message;
@@ -55,16 +56,16 @@ public class CallbackTests
         /* Just a bit of paranoia really, no reason to believe this wouldn't work.
          * You can delete this if you like, once the initial integration is complete */
 
-        LogSystem logSystem1 = new LogSystem();
+        using LogSystem logSystem1 = new LogSystem();
         Assert.True(logSystem1 != null);
-        LogSystem logSystem2 = new LogSystem();
+        using LogSystem logSystem2 = new LogSystem();
         Assert.True(logSystem2 != null);
 
         LogLevel? capturedLevel = null;
         string? capturedMessage = null;
         int timesCalled = 0;
 
-        ConnectedSpacesPlatformDotNet.LogCallback callback1 = new ConnectedSpacesPlatformDotNet.LogCallback((logLevel, message) =>
+        using ConnectedSpacesPlatformDotNet.LogCallback callback1 = new ConnectedSpacesPlatformDotNet.LogCallback((logLevel, message) =>
         {
             capturedLevel = logLevel;
             capturedMessage = message;
@@ -85,33 +86,5 @@ public class CallbackTests
         Assert.Equal(LogLevel.Warning, capturedLevel);
         Assert.Equal("Second call.", capturedMessage);
         Assert.Equal(2, timesCalled);
-    }
-
-    [Fact]
-    public void CallbackSurvivesGCInSameFrame()
-    {
-        LogSystem logSystem = new LogSystem();
-
-        LogLevel? capturedLevel = null;
-        string? capturedMessage = null;
-
-        logSystem.SetLogCallback(new ConnectedSpacesPlatformDotNet.LogCallback((logLevel, message) =>
-        {
-            capturedLevel = logLevel;
-            capturedMessage = message;
-        }));
-
-        // Try as hard as we can to get the GC to collect unreferenced objects
-        GC.Collect();
-        GC.WaitForPendingFinalizers();
-        GC.Collect();
-
-        // I actually don't fully understand why the `new ConnectedSpacesPlatformDotNet.LogCallback` isn't GC'd.
-        // It holds the delegate, and is held only in a WeakReference in a dict.
-        // (well, also a HandleRef for the scope of the P/Invoke call)
-        logSystem.LogMsg(LogLevel.Log, "Still works after GC?");
-        Assert.Equal(LogLevel.Log, capturedLevel);
-        Assert.Equal("Still works after GC?", capturedMessage);
-
     }
 }
