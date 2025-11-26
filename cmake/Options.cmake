05eed3a13aa1207657f9ee4eea98f2263b63e421
MAG-AlessioRegalbuto <alessio.regalbuto@magnopus.com>
2025-11-26 11:38:10 +0000

    Address PR feedback

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -4,6 +4,5 @@ include_guard(GLOBAL)
 
 # This is a blessed option, CMake knows about it and sets defaults based on if you declare it or not.
 option(BUILD_SHARED_LIBS "Build shared instead of static" ON)
-
 option(TRIM_CSP_NO_EXPORTS "Invoke Utilities/StripNoExports to trim the NO_EXPORT sections of CSP headers when consuming a release" ON)
-option(INIT_UNITY_TEST_PROJECT "Initialize the Unity test project with the latest generated classes and libraries for Unity integration tests" ON)
+option(COPY_OUTPUT_TO_UNITY_TEST_PROJECT "Copy the latest generated classes and libraries to the Unity test project" ON)
