3a32c48ebce6a614855f6ce01702b0025a80fe4b
MAG-AlessioRegalbuto <alessio.regalbuto@magnopus.com>
2025-12-03 13:10:48 +0000

    Fix path for Unity target folders and update gitignore
    Without this change, the libs would not be found on macos by Unity.

--- cmake/CopyToUnity.cmake
+++ cmake/CopyToUnity.cmake
@@ -7,7 +7,6 @@ cmake_path(APPEND CSP_LIB_UNITY_DIR
     "CspUnityTests"
     "Assets"
     "Plugins"
-    "CSP"
 )
 # 2 - Cache the result
 set(CSP_LIB_UNITY_DIR "${CSP_LIB_UNITY_DIR}"
@@ -30,20 +29,73 @@ set(CSP_ASMDEF_PATH "${_tmp_asmdef_path}"
 message(STATUS "CSP_LIB_UNITY_DIR='${CSP_LIB_UNITY_DIR}'")
 message(STATUS "CSP_ASMDEF_PATH='${CSP_ASMDEF_PATH}'")
 
-#
+# Base folder inside Unity project
+set(UNITY_CSP_ROOT "${CSP_LIB_UNITY_DIR}")
+
+message(STATUS "CMAKE_SYSTEM_NAME='${CMAKE_SYSTEM_NAME}'")
+message(STATUS "CMAKE_SYSTEM_PROCESSOR='${CMAKE_SYSTEM_PROCESSOR}'")
+message(STATUS "CMAKE_OSX_ARCHITECTURES='${CMAKE_OSX_ARCHITECTURES}'")
+
+if(APPLE)
+    if(IOS)
+        set(UNITY_PLATFORM_DIR "${UNITY_CSP_ROOT}/iOS")
+    elseif(CMAKE_SYSTEM_NAME STREQUAL "visionOS")
+        set(UNITY_PLATFORM_DIR "${UNITY_CSP_ROOT}/visionOS")
+    else()
+        # macOS (Intel or Apple Silicon)
+        if(CMAKE_OSX_ARCHITECTURES MATCHES "arm64")
+            set(UNITY_PLATFORM_DIR "${UNITY_CSP_ROOT}/macOS")
+        else()
+            set(UNITY_PLATFORM_DIR "${UNITY_CSP_ROOT}/macOSIntel")
+        endif()
+    endif()
+
+elseif(ANDROID)
+    # Android splits by ABI
+    if(CMAKE_ANDROID_ARCH_ABI STREQUAL "arm64-v8a")
+        set(UNITY_PLATFORM_DIR "${UNITY_CSP_ROOT}/android/arm64-v8a")
+    elseif(CMAKE_ANDROID_ARCH_ABI STREQUAL "armeabi-v7a")
+        set(UNITY_PLATFORM_DIR "${UNITY_CSP_ROOT}/android/armeabi-v7a")
+    elseif(CMAKE_ANDROID_ARCH_ABI STREQUAL "x86_64")
+        set(UNITY_PLATFORM_DIR "${UNITY_CSP_ROOT}/android/x86_64")
+    else()
+        message(FATAL_ERROR "Unsupported Android ABI: ${CMAKE_ANDROID_ARCH_ABI}")
+    endif()
+
+elseif(WIN32)
+    set(UNITY_PLATFORM_DIR "${UNITY_CSP_ROOT}/windows")
+
+elseif(UNIX AND NOT APPLE)
+    set(UNITY_PLATFORM_DIR "${UNITY_CSP_ROOT}/linux")
+
+else()
+    message(FATAL_ERROR "Unsupported platform for Unity plugin install.")
+endif()
+
+message(STATUS "Unity plugin output -> ${UNITY_PLATFORM_DIR}")
+
 # Run these operations during install:
 #   cmake --install build --config Debug
-#
 install(CODE "
+    message(\"Installing CSP to Unity plugin folder: ${UNITY_PLATFORM_DIR}\")
+    
     message(\"Deleting previous Unity generated code and libraries...\")
-    file(REMOVE_RECURSE \"${CSP_LIB_UNITY_DIR}\")
+    file(REMOVE_RECURSE \"${UNITY_PLATFORM_DIR}\")
+    file(REMOVE_RECURSE \"${UNITY_CSP_ROOT}/include\")
 
-    message(\"Creating required Unity folders...\")
-    file(MAKE_DIRECTORY \"${CSP_LIB_UNITY_DIR}\")
+    file(MAKE_DIRECTORY \"${UNITY_PLATFORM_DIR}\")
+    file(MAKE_DIRECTORY \"${UNITY_CSP_ROOT}/include\")
 
-    message(\"Copying CSP binary files to Unity...\")
-    file(COPY \"${INSTALL_DIR}/\" DESTINATION \"${CSP_LIB_UNITY_DIR}\")
+    # Copy C# SWIG bindings
+    message(\"Copying SWIG-generated C#...\")
+    file(COPY \"${INSTALL_DIR}/include/\" DESTINATION \"${UNITY_CSP_ROOT}/include\")
 
-    message(\"Copying asmdef into CSP plugin folder...\")
-    file(COPY \"${CSP_ASMDEF_PATH}\" DESTINATION \"${CSP_LIB_UNITY_DIR}/include/\")
+    # Copy platform-specific native library and binaries
+    message(\"Copying native library and binaries for Unity platform...\")
+    file(COPY \"${INSTALL_DIR}/bin/\" DESTINATION \"${UNITY_PLATFORM_DIR}\")
+    file(COPY \"${INSTALL_DIR}/lib/\" DESTINATION \"${UNITY_PLATFORM_DIR}\")
+    
+    # Copy asmdef
+    message(\"Copying asmdef...\")
+    file(COPY \"${CSP_ASMDEF_PATH}\" DESTINATION \"${UNITY_CSP_ROOT}/include\")
 ")
